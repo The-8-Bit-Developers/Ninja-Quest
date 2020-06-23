240a9e770a7160ac8d1d599b2393eb8741ce8984
SuperArtoo <arthur.owenson@gmail.com>
2020-06-23 19:29:26 +0100

    Update enemy.lua

--- res/enemy.lua
+++ res/enemy.lua
@@ -1,5 +1,5 @@
 -- The enemy variable is set in C++, as is the player variable
-
+first = true
 -- Load sprites in OnCreate
 function OnCreate()
 
@@ -12,18 +12,30 @@ end
 
 -- Main game loop - move player
 function OnUpdate(delta)
-
+	time = 0
+	
 	playerX = GetX(player)
 	enemyX = GetX(enemy)
 	totalX = playerX - enemyX
 	
+	--time += delta
+	--if(time == 500) then
+	AddY(enemy, -0.00000000000000000000000000001)
+	--	Time = 0
+	--end
+	if(first) then
+		SetY(enemy, 150)
+		first = false
+	end	
 	if(totalX < 0) then
 		AddForce(enemy, -10000, 0)
+		
 	else
 		AddForce(enemy, 10000, 0)
+		
 	end
 
-	SetY(enemy, enemy)
+	
 
 	-- Do stuff - look at 8BitEngine/include/LuaComponent.h to see all the functions you can do, and ask me :)
 	-- Eg SetX(sprite_id, X)
